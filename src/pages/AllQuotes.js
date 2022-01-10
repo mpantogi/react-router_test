cd8f72d8159256c7bfda2e1a49b002ecac081620
Christos Bantogias <xristos.bantogias@gmail.com>
2022-01-10 14:45:02 +0200

    add Route and Switch to the project

---
+++ src/pages/AllQuotes.js
@@ -0,0 +1,7 @@
+const AllQuotes = () => {
+    return (
+        <h1>All Quotes page</h1>
+    )
+};
+
+export default AllQuotes;
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,7 @@
+const QuoteDetail = () => {
+    return (
+        <h1>Quote detail page</h1>
+    )
+};
+
+export default QuoteDetail;
